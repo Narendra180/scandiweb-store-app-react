393df85fb8f096d6afa556b2d7845b29879a7772
narendra180 <narendrakanigiri18@gmail.com>
2023-06-11 18:28:59 +0530

    added gh-pages and new api end-point link

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import store from './redux/store';
 import { Provider } from 'react-redux'
 import App from './App';
@@ -13,7 +13,9 @@ import {
 } from "@apollo/client";
 
 export const apolloClient = new ApolloClient({
-  uri: 'http://localhost:4000/',
+  // uri: 'http://localhost:4000/',
+  // uri: "https://scw-graphql-backe-end.herokuapp.com/",
+  uri: "https://scw-task-backend.onrender.com/",
   cache: new InMemoryCache()
 });
 
@@ -23,9 +25,9 @@ root.render(
   <React.StrictMode>
     <ApolloProvider client={apolloClient}>
       <Provider store={store}>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </Provider>      
     </ApolloProvider>
   </React.StrictMode>
